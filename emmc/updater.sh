82e46650c350b0c8e70ac546c01280af0024f59b
Kyle Laplante <kyle.laplante@gmail.com>
2012-11-16 22:22:05 -0800

    update updater.sh

--- emmc/updater.sh
+++ emmc/updater.sh
@@ -1,11 +1,24 @@
 #!/system/bin/sh
 
-mkdir -p /mnt/sdcard/sk8sUpdater
-path=/mnt/sdcard/sk8sUpdater
+mount=/mnt/sdcard
 
+# needed for old updater.sh that had a typo
+if [ -d $mount/sk8sUdater ]
+then
+    mv $mount/sk8sUdater $mount/sk8sUpdater
+else
+    mkdir -p $mount/sk8sUpdater
+fi
+
+path=$mount/sk8sUpdater
+
+# delete the old configs
 rm -rf $path/*.txt
 
+# lets get the latest configs
+echo -e "\n"
 wget http://sk8erwitskil.selfip.net:81/url-$1.txt -O $path/url-$1.txt
+echo -e "\n"
 wget http://sk8erwitskil.selfip.net:81/version-$1.txt -O $path/version-$1.txt
 
 url=`cat $path/url-$1.txt`
@@ -13,10 +26,11 @@ version=`cat $path/version-$1.txt`
 
 if [ -e $path/$version ]
 then
-echo "You already have the latest version"
+    echo -e "\n"
+    echo "You already have the latest version"
 else
-wget $url -O $path/$version
-echo "install /emmc/sk8sUpdater/$version" > /cache/recovery/openrecoveryscript
-echo -e "\nYour phone will now reboot into recovery and install the latest version\n"
-reboot recovery
+    wget $url -O $path/$version
+    echo "install /emmc/sk8sUpdater/$version" > /cache/recovery/openrecoveryscript
+    echo -e "\nYour phone will now reboot into recovery and install the latest version\n"
+    reboot recovery
 fi
